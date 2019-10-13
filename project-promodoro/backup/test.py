a9d11e65016d8c9f7e6f06e7074ee0574d6cfa14
re4388 <re4388@gmail.com>
2019-10-13 23:21:02 +0800

    update

---
+++ project-promodoro/backup/test.py
@@ -0,0 +1,14 @@
+import re
+pattern = re.compile(r'\')  
+path = "D:/ben-code/python/pyqt5/project-promodoro/Brian.epub"
+
+def get_final_filename(f):
+    # slit up and get the part excluding extention
+    f = f.split(".")
+    filename = ".".join(f[0:-1])
+
+    # add new extension
+    processed_file_name = filename+".mobi"
+    return processed_file_name
+
+print(get_final_filename(path))
